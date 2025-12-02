ae6da2654f26b62ccd7a52d3befbaf9a9fb2c5a0
oliakazarinova-cmd <oliakokreidze@gmail.com>
2025-12-02 23:47:03 +0300

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request, jsonify
+
+app = Flask(__name__)
+
+# def validate_post_data(data: dict) -> bool
+
+
+
+# GET
+@app.route('/', methods=['GET'] )
+def hello():
+    return 'Hello World!'
+
+@app.route('/add', methods=['GET'] )
+def add():
+    try:
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+    except (TypeError, ValueError):
+        return 'a or b are not numbers'
+    return str(a + b)
+
+@app.route('/api', methods=['GET','POST'])
+def api():
+    if request.method == 'GET':
+        return jsonify({'status' : 'test'})
+    elif request.method == 'POST' :
+
+        return jsonify({'status' : 'OK!'})
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
